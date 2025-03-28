3a5c1b97666a9400d08011db3b119d0631b5756b
Septillihedron <resha3adnandaru@gmail.com>
2025-03-28 23:38:02 +0700

    Indentation to tabs

--- js/elements/section-elements/LazyLoadedSection.js
+++ js/elements/section-elements/LazyLoadedSection.js
@@ -5,47 +5,47 @@ import { Section } from "./Section.js"
 
 
 export class LazyLoadedSection extends Section {
-    
+	
 
-    /**
-     * @param {() => Entry[]} generator
-     */
-    constructor(generator) {
-        super([])
-        this.generator = generator
-        this.generated = false
-        this.button = this.makeGenerateButton()
-    }
+	/**
+	 * @param {() => Entry[]} generator
+	 */
+	constructor(generator) {
+		super([])
+		this.generator = generator
+		this.generated = false
+		this.button = this.makeGenerateButton()
+	}
 
-    makeGenerateButton() {
-        const button = document.createElement("button")
-        button.innerText = "+"
+	makeGenerateButton() {
+		const button = document.createElement("button")
+		button.innerText = "+"
 
-        button.addEventListener("click", () => {
-            if (!this.generated) this.generateEntries()
-            else this.removeEntries()
-            this.generated = !this.generated
-        })
+		button.addEventListener("click", () => {
+			if (!this.generated) this.generateEntries()
+			else this.removeEntries()
+			this.generated = !this.generated
+		})
 
-        this.children.unshift(new FocusableWrapper(button))
+		this.children.unshift(new FocusableWrapper(button))
 
-        return button
-    }
+		return button
+	}
 
-    generateEntries() {
-        this.clearChildren()
-        this.generator().forEach(this.addChild.bind(this))
-        this.focus()
-        this.focusNext()
-        this.button.innerText = "x"
-    }
+	generateEntries() {
+		this.clearChildren()
+		this.generator().forEach(this.addChild.bind(this))
+		this.focus()
+		this.focusNext()
+		this.button.innerText = "x"
+	}
 
-    removeEntries() {
-        this.clearChildren()
-        this.button.innerText = "+"
-    }
+	removeEntries() {
+		this.clearChildren()
+		this.button.innerText = "+"
+	}
 
-    /**
+	/**
 	 * @param {HTMLElement} parent
 	 */
 	toHTML(parent) {
@@ -53,25 +53,25 @@ export class LazyLoadedSection extends Section {
 		super.toHTML(parent)
 	}
 
-    /**
-     * @param {unknown} val
-     */
-    setValue(val) {
-        if (val == null) return
-        if (typeof val !== "object") {
-            incorrectTypeSetError(val)
-            return
-        }
-        Object.entries(val)
-            .forEach(([key, val]) => {
-                let entry = this.findEntryByKey(key)
-                if (!entry) {
-                    this.button.click()
-                    entry = this.findEntryByKey(key)
-                }
-                entry?.value.setValue(val)
-            })
-        this.unfocus()
-    }
+	/**
+	 * @param {unknown} val
+	 */
+	setValue(val) {
+		if (val == null) return
+		if (typeof val !== "object") {
+			incorrectTypeSetError(val)
+			return
+		}
+		Object.entries(val)
+			.forEach(([key, val]) => {
+				let entry = this.findEntryByKey(key)
+				if (!entry) {
+					this.button.click()
+					entry = this.findEntryByKey(key)
+				}
+				entry?.value.setValue(val)
+			})
+		this.unfocus()
+	}
 
 }
