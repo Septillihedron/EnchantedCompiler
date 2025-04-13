d212c42f0f061507b047a64e73a634801a85c435
Septillihedron <resha3adnandaru@gmail.com>
2025-04-13 21:26:10 +0700

    Make PropertiesMap error for same keys

--- js/elements/section-elements/PropertiesMap.js
+++ js/elements/section-elements/PropertiesMap.js
@@ -1,12 +1,16 @@
+import { applyFunctionToConstructor as applyFunctionsToConstructor } from "../constructor-operators.js"
 import { createElement } from "../createHtmlElement.js"
+import { errorLevels, Input } from "../value-elements.js"
 import { FocusableWrapper, YamlElement } from "../yaml-element.js"
 import { Entry } from "./Entry.js"
 import { Section } from "./Section.js"
 
+const SAME_KEY_ERROR = "Duplicate object keys"
+
 
 export class PropertiesMap extends Section {
 	/**
-	 * @param {(name: string | number) => (parent: YamlElement) => Entry} addfn
+	 * @param {(name: string | number) => (parent: YamlElement) => Entry<Input, ?>} addfn
 	 */
 	constructor(parent, addfn) {
 		super(parent, [])
@@ -40,25 +44,52 @@ export class PropertiesMap extends Section {
 	}
 
 	/**
-	 * @param {(parent: YamlElement) => Entry} element 
+	 * @param {(parent: YamlElement) => Entry<Input, ?>} element 
 	 */
 	addChild(element) {
-		super.addChild(parent => {
-			const entry = element(parent)
-			const removeButton = createElement(this, "button")
-			removeButton.innerText = "x"
-			removeButton.addEventListener("click", () => {
-				const elementIndex = this.children.findIndex(child => entry == child)
-				this.children.splice(elementIndex, 1)
-				this.values.splice(elementIndex-1, 1)
-				this.container.removeChild(entry.container)
-				if (this.focusIndex >= elementIndex) {
-					this.focusIndex--
-				}
-				if (this.focusIndex != -1) this.children[this.focusIndex].focus()
+		super.addChild(applyFunctionsToConstructor(element, 
+			this.addRemoveButtonToEntry.bind(this), 
+			this.addNoSameKeyValidator.bind(this)
+		))
+	}
+
+	/**
+	 * @param {Entry} entry
+	 */
+	addRemoveButtonToEntry(entry) {
+		const removeButton = createElement(this, "button")
+		removeButton.innerText = "x"
+		removeButton.addEventListener("click", () => {
+			const elementIndex = this.children.findIndex(child => entry == child)
+			this.children.splice(elementIndex, 1)
+			this.values.splice(elementIndex-1, 1)
+			this.container.removeChild(entry.container)
+			if (this.focusIndex >= elementIndex) {
+				this.focusIndex--
+			}
+			if (this.focusIndex != -1) this.children[this.focusIndex].focus()
+		})
+	}
+
+	/**
+	 * @param {Entry<Input, ?>} thisEntry
+	 */
+	addNoSameKeyValidator(thisEntry) {
+		const input = thisEntry.key
+		input.addValidator(thisValue => {
+			const shouldError = this.values.some(entry => {
+				if (entry == thisEntry) return false
+				if (entry.key.getValue() == thisValue) return true
 			})
-			entry.addRemoveButton(removeButton)
-			return entry
+			if (shouldError) {
+				return {
+					level: errorLevels.error,
+					message: SAME_KEY_ERROR
+				}
+			}
+		})
+		input.addChangedListener(() => {
+			this.values.forEach(entry => entry.key.validate())
 		})
 	}
 
